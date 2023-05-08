bcc4fb71c3e1334a7c7320fde6ed9e2fdad18c33
vivganes <vivganes@users.noreply.github.com>
2023-05-08 19:16:19 +0000

    deploy: 477022eafbabf5a7422d952ca5252f23c53e7606

--- src/app/component/single-post/single-post.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-single-post',
-  templateUrl: './single-post.component.html',
-  styleUrls: ['./single-post.component.scss'],
-})
-export class SinglePostComponent {}
