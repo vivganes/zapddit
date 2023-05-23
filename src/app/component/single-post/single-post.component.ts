b6d25fe56d9bbbb2110bed0dd6e7d234fc8be444
vivganes <caliberoviv@gmail.com>
2023-05-23 21:52:49 +0530

    auto open comments in single post view

--- src/app/component/single-post/single-post.component.ts
+++ src/app/component/single-post/single-post.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NDKEvent } from '@nostr-dev-kit/ndk';
 import { NdkproviderService } from 'src/app/service/ndkprovider.service';
