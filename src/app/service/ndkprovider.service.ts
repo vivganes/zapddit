5c2f20f6061e006f0035878878aed590ef4e5d64
Kamal Raj Sekar <notify.kamalraj@gmail.com>
2023-06-02 10:56:04 +0100

    incorporated feedback

--- src/app/service/ndkprovider.service.ts
+++ src/app/service/ndkprovider.service.ts
@@ -308,7 +308,7 @@ export class NdkproviderService {
                 pictureUrl: item.profile?.image!,
                 about:item.profile?.about!
               }, item.npub)
-              console.log("user added to - "+JSON.stringify(table))
+              console.log("user added to - "+ table.name)
             }else{
               console.log("User already exists")
             }
@@ -347,6 +347,7 @@ export class NdkproviderService {
         mutedNDKUsers.push(ndkUser);
 
         if(mutedNDKUsers.length == i){
+            console.log("PeopleIMuted from relay "+ i);
             this.peopleIMuted.next(mutedNDKUsers);
         }
       })
@@ -374,7 +375,6 @@ export class NdkproviderService {
     console.log("PeopleIMuted from cache "+peopleIMutedFromCache?.length);
 
     var peopleIMutedFromRelay = await this.fetchMuteList(this.currentUser?.hexpubkey()!);
-    console.log("PeopleIMuted from relay "+peopleIMutedFromRelay?.length);
 
     if((peopleIMutedFromCache?.length === 0) || peopleIMutedFromCache?.length !== peopleIMutedFromRelay?.length){
         await this.fetchMutedPeopleAndCache(peopleIMutedFromRelay);
