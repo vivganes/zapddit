9cab1c3bf4853dfa5eef31f5d369d518d0e8c69c
Kamal Raj Sekar <notify.kamalraj@gmail.com>
2023-07-06 13:44:50 +0100

    new community creation related screens and logic (#47)
    * new community creation related screens and logic
    
    * Update community.service.ts
    
    using same tagnames as other clients
    
    * Update community-list.component.html
    
    Remove emoji icon from button and use cds-icon
    
    * Update create-community.component.html
    
    ---------
    
    Co-authored-by: Vivek Ganesan <caliberoviv@gmail.com>

--- src/app/model/community.ts
+++ src/app/model/community.ts
@@ -3,8 +3,10 @@ import { NDKUserProfile } from "@nostr-dev-kit/ndk";
 export interface Community{
     creatorHexKey?:string,
     name?:string,
+    displayName?:string,
     id?:string,
     description?:string,
+    rules?:string;
     image?:string,
     creatorProfile?:NDKUserProfile,
     moderatorHexKeys?:string[]
